28aae94690664fd887f4445dab3d63a9b8d9c11c
Mehmet Rasid Gencosmanoglu <mrgenco@gmail.com>
2021-04-22 13:52:51 +0300

    api request

--- ui/src/components/ImageCard.js
+++ ui/src/components/ImageCard.js
@@ -1,38 +1,37 @@
-const ImageCard = () => {
+const ImageCard = ({ image }) => {
+    const tags = image.tags.split(',');
+  
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-
-            <div className="px-6 py-4">
-                <div  className="py-2 font-bold text-purple-500 text-xl">
-                Photo by John Doe
-                </div>
-                <ul>
-                <li>
-                <strong >Size: </strong>
-                    4000
-                </li>
-                <li >
-                    <strong className="underline">Description</strong>
-                    <br/>
-                    All those moments will be lost in time.
-                </li>
-                </ul>
-            </div>
-            <div className="px-6 py-4">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag1
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag2
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #tag3
-                </span>
-            </div>
-
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img src={image.webformatURL} alt="" className="w-full"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-500 text-xl mb-2">
+            Photo by {image.user}
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>
+              {image.views}
+            </li>
+            <li>
+              <strong>Downloads: </strong>
+              {image.downloads}
+            </li>
+            <li>
+              <strong>Likes: </strong>
+              {image.likes}
+            </li>
+          </ul>
         </div>
+        <div className="px-6 py-4">
+          {tags.map((tag, index) => (
+            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #{tag}
+          </span>
+          ))}
+        </div>
+      </div>
     )
-}
-
-export default ImageCard
+  }
+  
+  export default ImageCard;
