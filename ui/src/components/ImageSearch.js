d47ac880971befa42baedbfa9ffe70691cdaf0b2
mrgenco <mrgenco@gmail.com>
2021-04-25 12:28:53 +0300

    button color changes

--- ui/src/components/ImageSearch.js
+++ ui/src/components/ImageSearch.js
@@ -10,7 +10,7 @@ const ImageSearch = ({ searchText }) => {
   };
 
   return (
-    <div className="max-w-sm rounded overflow-hidden my-5 mx-auto">
+    <div className="max-w-sm rounded overflow-hidden mt-20 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex px-2 bg-gray-100 items-center border-b border-b-2 border-teal-500 py-2">
           <input
@@ -20,7 +20,7 @@ const ImageSearch = ({ searchText }) => {
             placeholder="Search Image Term..."
           />
           <button
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
             type="submit"
           >
             Search
--- ui/src/components/ImageUpload.js
+++ ui/src/components/ImageUpload.js
@@ -3,9 +3,9 @@ const ImageUpload = () => {
     console.log("Upload button clicked");
   };
   return (
-    <div className="max-w-sm rounded content-center items-center mx-auto ">
+    <div className="max-w-sm rounded content-center  mt-5 mb-20 items-center mx-auto ">
       <button
-        className="w-full inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        className="w-full inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
         onClick={onClick}
       >
         Upload New Image
